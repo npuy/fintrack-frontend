e6986bfbb35cd4a968fe50c3b982be9536162470
Nicolas Pereira <nicolas.pp333@gmail.com>
2026-04-25 20:12:06 -0300

    Fix date handling in EditTransaction component to use client side timezone

--- app/components/Transaction/EditTransaction.tsx
+++ app/components/Transaction/EditTransaction.tsx
@@ -1,24 +1,17 @@
 import { Container, Fieldset, Space, Title } from "@mantine/core";
 import { Form, useLoaderData } from "@remix-run/react";
-import { useEffect, useState } from "react";
 import { loader } from "~/routes/transactions.$transactionId";
 import DeleteButton from "../Buttons/DeleteButton";
 import EditButton from "../Buttons/EditButton";
 import { FormTransaction } from "./FormTransaction";
-import { parseYYYYMMDDToDate } from "~/utils/dates";
 
 export default function EditTransaction() {
   const data = useLoaderData<typeof loader>();
-  const [date, setDate] = useState<Date>();
-
-  useEffect(() => {
-    setDate(parseYYYYMMDDToDate(data.transaction.date));
-  }, [data.transaction.date]);
 
   const loadData = {
     description: data.transaction.description,
     amount: data.transaction.amount,
-    date,
+    date: data.transaction.date,
     account: data.transaction.accountId,
     category: data.transaction.categoryId,
     type: data.transaction.type.toString(),
